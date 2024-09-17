88459556cc66479a68457e23ced3d2e4155ebca7
tim.bloyce <bassist_bloyce@hotmail.com>
2024-09-17 11:21:33 +0100

    Revert "random_generator.py - added subtract logic"
    This reverts commit 3c2755eca55cc79c284ba94c99693112f4d1d2e3.

--- random_generator.py
+++ random_generator.py
@@ -1,12 +1,14 @@
 import random
 
 num = round(random.uniform(0,100),1)
-other_num = round(random.uniform(0,100),1)
-diff = round(num - other_num,1)
 
+<<<<<<< HEAD
 while num > other_num:
     print('calculating')
 
 
 print(f'Your random percentage is {num}%, '
           f'which is {diff}% different to {other_num}%')
+=======
+print(f'Your random percentage is {num}%')
+>>>>>>> parent of 3c2755e (random_generator.py - added subtract logic)
