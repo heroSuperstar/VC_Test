3f468760b6ee7941c5b5b3f7b16409ea1fec5c15
tim.bloyce <bassist_bloyce@hotmail.com>
2024-09-17 14:01:16 +0100

    CHANGE - added new input to accept the result from the loop

--- random_generator.py
+++ random_generator.py
@@ -3,6 +3,23 @@ import random
 num = round(random.uniform(0,100),1)
 other_num = round(random.uniform(0,100),1)
 diff = round(num - other_num,1)
+ackno = False
 
+# Print numbers
 print(f'Your random percentage is {num}%, '
           f'which is {diff}% different to {other_num}%')
+
+
+while ackno == False:
+    ans = bool(input('Accept the truth:'))
+
+    if ans != True:
+        print('You have not accepted the truth.')
+    
+    else:
+        print('Good, now go and defy your fate!')
+        ackno = ans
+
+
+
+
