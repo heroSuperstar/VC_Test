124437279c9e28266322898885711e070e3af4bb
tim.bloyce <bassist_bloyce@hotmail.com>
2024-09-17 11:23:31 +0100

    random_generator.py - fixed calculation error

--- random_generator.py
+++ random_generator.py
@@ -1,14 +1,9 @@
 import random
 
 num = round(random.uniform(0,100),1)
-
-<<<<<<< HEAD
-while num > other_num:
-    print('calculating')
-
+other_num = round(random.uniform(0,100),1)
+diff = round(num - other_num,1)
 
 print(f'Your random percentage is {num}%, '
           f'which is {diff}% different to {other_num}%')
-=======
-print(f'Your random percentage is {num}%')
->>>>>>> parent of 3c2755e (random_generator.py - added subtract logic)
+
