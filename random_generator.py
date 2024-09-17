3c2755eca55cc79c284ba94c99693112f4d1d2e3
tim.bloyce <bassist_bloyce@hotmail.com>
2024-09-17 11:16:27 +0100

    random_generator.py - added subtract logic

--- random_generator.py
+++ random_generator.py
@@ -1,5 +1,8 @@
 import random
 
 num = round(random.uniform(0,100),1)
+other_num = round(random.uniform(0,100),1)
+diff = round(num - other_num,1)
 
-print(f'Your random percentage is {num}%')
+print(f'Your random percentage is {num}%, '
+      f'which is {diff}% different to {other_num}%')
