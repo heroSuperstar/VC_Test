7d660b61648c939f909f0bfaddbe7746f080a35e
tim.bloyce <bassist_bloyce@hotmail.com>
2024-09-17 11:20:22 +0100

    random_generator.py - added while loop

--- random_generator.py
+++ random_generator.py
@@ -4,5 +4,9 @@ num = round(random.uniform(0,100),1)
 other_num = round(random.uniform(0,100),1)
 diff = round(num - other_num,1)
 
+while num > other_num:
+    print('calculating')
+
+
 print(f'Your random percentage is {num}%, '
-      f'which is {diff}% different to {other_num}%')
+          f'which is {diff}% different to {other_num}%')
